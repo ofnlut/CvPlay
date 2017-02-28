6def78509a634f7ec25233c8ad22873702440343
PurposeinthePause <dohrraba3@gmail.com>
2017-02-28 18:07:00 -0500

    Add video tracking

--- faces.py
+++ faces.py
@@ -4,27 +4,53 @@ import argparse
 
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to image")
+ap.add_argument("-i", "--image", required=False, help="Path to image")
+ap.add_argument("-v", "--video", required=False, help="video or no?")
 args = vars(ap.parse_args())
 
 #load up our cascades
 face_cascade = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("cascades/haarcascade_eye.xml")
 
+
 #read and convert our imgage to grayscale
-img = cv2.imread(args["image"])
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+if args["image"]:
+
+    img = cv2.imread("images/"+args["image"])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x, y, w, h) in faces:
-    cv2.rectangle(img,(x,y),(x+w,y+h), (255,0,0), 2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
 
-cv2.imshow("img",img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow("img",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+if args["video"]:
+    vid = cv2.VideoCapture(int(args["video"]))
+
+    while True:
+        ret, frame = vid.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h), (255,0,0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
+        cv2.imshow('video',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    vid.release()
+    cv2.destroyAllWindows()
