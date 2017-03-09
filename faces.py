2a68648a4725d5e44a1e291e84e45e6a72957394
PurposeinthePause <dohrraba3@gmail.com>
2017-03-09 17:38:18 -0500

    add imgurl method*Broken*

--- faces.py
+++ faces.py
@@ -1,10 +1,13 @@
 import numpy as np
 import cv2
 import argparse
+import urllib.request
 
 
 ap = argparse.ArgumentParser()
+ap.add_argument("--foo", help='foo help')
 ap.add_argument("-i", "--image", required=False, help="Path to image")
+ap.add_argument("-url", "--imgurl", required=False, help="Url link to image")
 ap.add_argument("-v", "--video", required=False, help="video or no?")
 args = vars(ap.parse_args())
 
@@ -32,6 +35,28 @@ if args["image"]:
     cv2.imshow("img",img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+if args["imgurl"]:
+    response = urllib.request.urlopen(args["imgurl"])
+    translate = np.array(bytearray(response.read()),dtype=np.uint8)
+    translate = cv2.imdecode(translate, cv2.IMREAD_COLOR)
+
+    img = cv2.imread(translate)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh), (0,255,0),2)
+
+    cv2.imshow("img",img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
 if args["video"]:
     vid = cv2.VideoCapture(int(args["video"]))
